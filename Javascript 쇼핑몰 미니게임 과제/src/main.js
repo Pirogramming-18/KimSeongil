a977f9458274c4b6a6c12f7ecb3ae2aad1d7e335
hongcana <hongcana@kakao.com>
2023-01-04 21:24:51 +0900

    JavaScript - display items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -5,13 +5,29 @@ function loadItems() {
     .then((json) => json.items); // JSON 안에 있는 items return
 }
 
+function displayItems(items) {
+  const container = document.querySelector(".items");
+  const html = items.map((item) => createHTMLString(item));
+  console.log(html);
+  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+}
+
+function createHTMLString(item) {
+  return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
 // main
 // promise가 성공적으로 아이템을 return하면..
 // 그렇지 않으면 catch로 문제 파악
 loadItems()
   .then((items) => {
     console.log(items);
-    // displayItems(items);
+    displayItems(items);
     // setEventListeners(items);
   })
   .catch(console.log);
