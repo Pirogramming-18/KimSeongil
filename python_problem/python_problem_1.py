ab47552fa358ee2d73f4c8e7bba332d0598bddbd
hongcana <hongcana@kakao.com>
2023-01-06 01:28:26 +0900

    1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -10,6 +10,20 @@ while True:
     except ValueError:
         print('정수를 입력하세요.')
 
-for num in range(0, input_num):
+for _ in range(input_num):
     num += 1
     print(f'playerA : {num}')
+
+while True:
+    try:
+        input_num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+        if 1 <= input_num <= 3:
+            break
+        else:
+            print('1,2,3 중 하나를 입력하세요.')
+    except ValueError:
+        print('정수를 입력하세요.')
+
+for _ in range(input_num):
+    num += 1
+    print(f'playerB : {num}')
