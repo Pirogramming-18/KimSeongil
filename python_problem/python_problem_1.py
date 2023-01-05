e131d05671e0c4bfe1590e5e3e7c8f30de9d975c
hongcana <hongcana@kakao.com>
2023-01-06 01:24:01 +0900

    1-4

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -9,3 +9,7 @@ while True:
             print('1,2,3 중 하나를 입력하세요.')
     except ValueError:
         print('정수를 입력하세요.')
+
+for num in range(0, input_num):
+    num += 1
+    print(f'playerA : {num}')
